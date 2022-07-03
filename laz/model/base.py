4d7127c309952bbc36c450be6ba7cf6cd14aa752
Josh Wycuff <Joshua.Wycuff@turner.com>
2022-07-03 13:28:31 -0400

    fix: convert env values to string on load

--- laz/model/base.py
+++ laz/model/base.py
@@ -1,6 +1,8 @@
 # std
 from __future__ import annotations
 import json
+
+# external
 import yaml
 from yaml.loader import SafeLoader
 
--- laz/model/configuration.py
+++ laz/model/configuration.py
@@ -3,6 +3,10 @@ from __future__ import annotations
 import os
 from typing import List
 
+# external
+import yaml
+from yaml.loader import SafeLoader
+
 # internal
 from laz.model.base import BaseObject
 from laz.model.target import Target
@@ -24,6 +28,16 @@ class Configuration(BaseObject):
 
     def get_target(self, name: str) -> Target:
         return Target(name, **self.data.get('targets', {}).get(name, {}) or {})
+
+    @classmethod
+    def deserialize(cls, id: str, serialized: str) -> Configuration:
+        data = yaml.load(serialized, Loader=SafeLoader) or {}
+        if 'env' in data:
+            if data['env'] is None:
+                data['env'] = {}
+            assert isinstance(data['env'], dict)
+            data['env'] = {k: str(v) for k, v in data['env'].items()}
+        return cls(id, **data)
 
     @classmethod
     def load(cls, filepath: str) -> Configuration:
